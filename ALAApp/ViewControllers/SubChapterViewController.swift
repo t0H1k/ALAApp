07735d87ed4694dc03167a0bf091e89eb392a807
Anton Boev <110462969+t0H1k@users.noreply.github.com>
2022-10-26 23:40:36 +0300

    Добавил информации в дату с целью добиться передачи информации. Добавил вью файл для отображения текста в него Там поковырял, сям поковырял.

--- ALAApp/ViewControllers/SubChapterViewController.swift
+++ ALAApp/ViewControllers/SubChapterViewController.swift
@@ -10,19 +10,19 @@ import UIKit
 class SubChapterViewController: UITableViewController {
     
     let subChapter = SubChapterBook.getSubChapter()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         //navigationItem.backBarButtonItem
     }
     
-
+    
     // MARK: - Table view data source
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return subChapter.count
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "subChapter", for: indexPath)
         let chapter = subChapter[indexPath.row]
@@ -31,17 +31,18 @@ class SubChapterViewController: UITableViewController {
         cell.contentConfiguration = content
         return cell
     }
-
+    
     @IBAction func backButton(_ sender: Any) {
         dismiss(animated: true)
     }
     
     
     // MARK: - Navigation
-
-//    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    
 //    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//        // Get the new view controller using segue.destination.
-//        // Pass the selected object to the new view controller.
+//        if let indexPath = tableView.indexPathForSelectedRow {
+//            guard let detailVC = segue.destination as? DetailsSubChapterViewController else { return }
+//            detailVC.detailChapter = subChapter[indexPath.row]
+//        }
 //    }
 }
