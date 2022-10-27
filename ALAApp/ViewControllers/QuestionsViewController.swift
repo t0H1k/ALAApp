154ec7c95b2f36c932bff6772f182b4412e208ba
Aitolkun <totita@list.ru>
2022-10-27 19:33:16 +0300

    optionButton

--- ALAApp/ViewControllers/QuestionsViewController.swift
+++ ALAApp/ViewControllers/QuestionsViewController.swift
@@ -9,14 +9,46 @@ import UIKit
 
 class QuestionsViewController: UIViewController {
     
- 
+    
     @IBOutlet var radioButtons: [UIButton]!
-
-
+    
+    @IBOutlet var answerLabels: [UILabel]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
     }
     
+    @IBAction func radioButtonSelected(_ sender: UIButton) {
+        
+        if sender.tag == 0 {
+            radioButtons[0].isSelected = true
+            radioButtons[1].isSelected = false
+            radioButtons[2].isSelected = false
+            radioButtons[3].isSelected = false
+        } else if sender.tag == 1 {
+            radioButtons[0].isSelected = false
+            radioButtons[1].isSelected = true
+            radioButtons[2].isSelected = false
+            radioButtons[3].isSelected = false
+
+        }
+        else if sender.tag == 2 {
+            radioButtons[0].isSelected = false
+            radioButtons[1].isSelected = false
+            radioButtons[2].isSelected = true
+            radioButtons[3].isSelected = false
+        } else if sender.tag == 3 {
+            radioButtons[0].isSelected = false
+            radioButtons[1].isSelected = false
+            radioButtons[2].isSelected = false
+            radioButtons[3].isSelected = true
+        } else {
+            radioButtons[0].isSelected = false
+            radioButtons[1].isSelected = false
+            radioButtons[2].isSelected = false
+            radioButtons[3].isSelected = false
+        }
+    }
 }
+
