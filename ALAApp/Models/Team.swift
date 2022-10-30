180c3df3e0d3cb06fd989833187352456faeaf9b
Aitolkun <totita@list.ru>
2022-10-30 14:49:07 +0300

    conflict

--- ALAApp/Models/Team.swift
+++ ALAApp/Models/Team.swift
@@ -9,10 +9,10 @@ struct Team {
     
     let aitolkun = "Айтолкун Анарбекова"
     let conflict = "dkjfhdksjf"
-    let leonid = "Леонид Горбунов"
     let anton = "Антон Боев"
     let aitolkunPhoto = "Aitolkun"
     let leonidPhoto = "Leonid"
     let antonPhoto = "Anton"
+    
 }
 
