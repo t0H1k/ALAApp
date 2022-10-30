f1cd40b9d7eea002458d2cc4de6dd06a7ed02083
Aitolkun <totita@list.ru>
2022-10-30 15:07:51 +0300

    ввв

--- ALAApp/Models/Team.swift
+++ ALAApp/Models/Team.swift
@@ -7,14 +7,7 @@
 
 struct Team {
     
-    let aitolkun = "Айтолкун Анарбекова"
-    let conflict = "dkjfhdksjf"
-    let tjkd = "dddddcvcvcvc"
-    let dkj = "fff"
-    let anton = "Антон Боев"
-    let aitolkunPhoto = "Aitolkun"
-    let leonidPhoto = "Leonid"
-    let antonPhoto = "Anton"
+    
     
 }
 
